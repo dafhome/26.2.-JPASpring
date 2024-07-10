28ab6c002397d486c8d792351c7236f9abba81b8
dafhome <d.fernalvarez@gmail.com>
2024-07-10 19:02:51 +0200

    commit

---
+++ tienda_plantas/src/main/java/com/tiendaplantas/tienda_plantas/TiendaPlantasApplication.java
@@ -0,0 +1,13 @@
+package com.tiendaplantas.tienda_plantas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaPlantasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaPlantasApplication.class, args);
+	}
+
+}
---
+++ tienda_plantas/src/test/java/com/tiendaplantas/tienda_plantas/TiendaPlantasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendaplantas.tienda_plantas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaPlantasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
